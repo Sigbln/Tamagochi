134edf9900257dfa1c74f1b8221206cd444a3d4b
Unsigned <76623683+Sigbln@users.noreply.github.com>
2021-04-08 00:28:22 +0300

    Update HUD.py

--- HUD.py
+++ HUD.py
@@ -5,7 +5,7 @@ import global_names
 pygame.init()
 
 font1 = pygame.font.Font(None, 15)  # Размер для HUD
-font2 = pygame.font.Font(None, 25)
+font2 = pygame.font.Font(None, 25)  # тоже
 ABot = [font2.render("Botayem .", True, global_names.WHITE),
         font2.render("Botayem ..", True, global_names.WHITE),
         font2.render("Botayem ...", True, global_names.WHITE)]
