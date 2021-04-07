5d9f4cb911a24ca30acf7ceaa987a3ee5da14b77
Unsigned <76623683+Sigbln@users.noreply.github.com>
2021-04-08 00:24:29 +0300

    Update saver.py

--- saver.py
+++ saver.py
@@ -11,21 +11,21 @@ import pickle
 в не игры, за 12 часов вне игры показатели падают на 40 пунктов
 """
 
-
+# загружаем сохранение и уменьшаем показатели
 def load():
     if os.path.exists(global_names.MY_PATH) and os.path.getsize(
             global_names.MY_PATH) > global_names.EMPTY:
         with open(global_names.MY_PATH, "rb") as f:
             data = pickle.load(f)
-        coef_decrease = int((time.time() - data["time"]) / (3600 * 24) * 40)
+        coef_decrease = int((time.time() - data["time"]) / global_names.COEF)
         global_names.pet.eat = data["eat"] - coef_decrease
-        if global_names.pet.eat < 0:
+        if global_names.pet.eat < global_names.EMPTY:
             global_names.pet.hp += global_names.pet.eat
-            global_names.pet.eat = 0
+            global_names.pet.eat = global_names.EMPTY
         global_names.pet.sleep = data["sleep"] - coef_decrease
-        if global_names.pet.sleep < 0:
+        if global_names.pet.sleep < global_names.EMPTY:
             global_names.pet.hp += global_names.pet.sleep
-            global_names.pet.sleep = 0
+            global_names.pet.sleep = global_names.EMPTY
         global_names.pet.bot = data["bot"] - coef_decrease
 
 
